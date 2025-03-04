f762fb4c59509e4c6ade05953f9388dd4a67ca71
Souza284 <kanaan.pedro2@gmail.com>
2025-03-04 15:29:29 -0300

    .

---
+++ Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿#region Unidimensional Arrays
+
+int[] numbers = new int[5];
+
+numbers[0] = 4;
+numbers[1] = 7;
+numbers[2] = 23;
+numbers[3] = 432;
+numbers[4] = 8;
+numbers[4] = 77;
+
+Console.WriteLine(numbers[3]);
+
+#endregion
+
