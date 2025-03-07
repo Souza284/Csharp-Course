73b1dfc14c17a61537d73364f6a4a07e28c6cbf0
Souza284 <kanaan.pedro2@gmail.com>
2025-03-07 14:51:14 -0300

    Classes&Objects1

---
+++ Classes&Objects/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyClass myClass01 = new();
+    }
+}
+
+class MyClass
+{
+
+}
--- Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
+++ Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e7dae0ae3c1ae9af138bd19f604c6aba2fbc741")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f829395b0e5a85ff4f3bfc3c132efc3465003ac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
